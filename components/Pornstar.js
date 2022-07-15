ae59aa7409e454fad82b303df65a4af5852850d3
Manoj <manojkumarprayasjee47@gmail.com>
2022-07-15 00:35:39 -0800

    ui improved

--- components/Pornstar.js
+++ components/Pornstar.js
@@ -5,6 +5,7 @@ import Info from "./Info";
 
 const Pornstar = ({ pornstars,next }) => {
   let randomIndex=Math.floor(Math.random()*5)
+  console.log(pornstars[0].images[0].image_link)
   return (
     <div className=" max-w-6xl mx-auto md:p-2 rounded">
       <div className="grid p-1 md:p-3 gap-2 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
